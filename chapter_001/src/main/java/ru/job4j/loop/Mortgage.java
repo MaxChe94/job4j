8eb0eb4688ddb0d7f0aea8185f7e2dfc2de14b82
MaxChe94 <mchere94@gmail.com>
2019-11-03 19:10:30 +0500

    5.7. Ипотека.[#191823]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        percent/=100;
+        double debt = amount + amount*percent;
+        int year = 0;
+        while (debt>0){
+            debt-=salary;
+            year++;
+            debt*=(1+percent);
+        }
+        return year;
+    }
+}
