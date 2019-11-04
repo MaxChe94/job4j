46ce1549e743dff561f5fdb23ee1df613fe84da3
MaxChe94 <mchere94@gmail.com>
2019-11-05 01:17:45 +0500

    Правка 6.3. Массив заполнен true или false[#191831]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,11 +2,13 @@ package ru.job4j.array;
 
 public class Check {
         public boolean mono(boolean[] data) {
-            boolean result = false;
-            for (int i=0;i<data.length;i++){
-                result = (data[0] == data[i]);
-                if (!result) break;
+            boolean result = true;
+            for (int i=0;i<data.length;i++) {
+                if (data[0] != data[i]) {
+                    result = false;
+                    break;
                 }
+            }
             return result;
         }
 }
