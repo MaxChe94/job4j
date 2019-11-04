91bf2c57acfd006eac5711009e580a0c37d34116
MaxChe94 <mchere94@gmail.com>
2019-11-04 20:36:28 +0500

    Правка 5.5. Простое число.[#191824]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,11 +2,16 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public boolean check(int num){
-        boolean res = false;
-        for (int i=2;i<num;i++){
-            if (res=(num%i != 0)) continue;
-            res=false;
-            break;
+        boolean res = true;
+        if (num < 2) {
+            res = false;
+        } else {
+            for (int i = 2;i < num; i++) {
+                if (num % i == 0) {
+                    res = false;
+                    break;
+                }
+            }
         }
         return res;
     }
