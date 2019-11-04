c0186fc8a36e1260dcbdaf000d90165e3b3b2af4
MaxChe94 <mchere94@gmail.com>
2019-11-04 21:22:09 +0500

    6.4. Слово начинается с ...[#191830]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+        for (int i=0; i<pref.length;i++){
+            result = (word[i] == pref[i]);
+             if (!result) break;
+        }
+        return result;
+    }
+}
