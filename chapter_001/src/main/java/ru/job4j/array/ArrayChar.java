053a941d7f804eb2d086f7857b19dbd27f85b5d4
MaxChe94 <mchere94@gmail.com>
2019-11-04 23:04:21 +0500

    Правка 6.4. Слово начинается с ...[#191830]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,10 +2,12 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
+        boolean result = true ;
         for (int i=0; i<pref.length;i++){
-            result = (word[i] == pref[i]);
-             if (!result) break;
+            if (word[i] != pref[i]){
+                result = false;
+                break;
+            }
         }
         return result;
     }
