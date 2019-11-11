89895e1456a51683c00bf87048199226fe2a982b
MaxChe94 <mchere94@gmail.com>
2019-11-11 23:59:56 +0500

    Правка Объединить два массива [#124482]

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -4,7 +4,16 @@ import java.util.Arrays;
 
 public class Merge {
 
-    public int[] merge(int[] left, int[] right) {
+    public static void main (String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[]{1, 3, 5},
+                new int[]{2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+
+    public int[] merge (int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         int i = 0;
         int j = 0;
@@ -12,34 +21,19 @@ public class Merge {
         boolean leftend = left.length < 1;
         boolean rightend = right.length < 1;
         while (!leftend || !rightend) {
-            if (!leftend) {
-                 if (!rightend) {
-                     if (left[i] < right[j]) {
-                         rsl[k] = left[i];
-                         leftend = ++i == left.length;
-                     } else {
-                         rsl[k] = right[j];
-                         rightend = ++j == right.length;
-                     }
-                 } else {
-                     rsl[k] = left[i];
-                     leftend = ++i == left.length;
-                 }
-            } else if (!rightend) {
-                rsl[k] = right[j];
-                rightend = ++j == right.length;
+            if ((!leftend) && (rightend || left[i] <= right[j])) {
+                rsl[k] = left[i];
+                i++;
+                leftend = (i == left.length);
+                k++;
             }
-            k++;
+            if ((!rightend) && (leftend || left[i] >= right[j])) {
+                rsl[k] = right[j];
+                j++;
+                rightend = (j == right.length);
+                k++;
+            }
         }
         return rsl;
-    }
-
-    public static void main(String[] args) {
-        Merge process = new Merge();
-        int[] rsl = process.merge(
-                new int[] {1, 3, 5},
-                new int[] {2, 4}
-        );
-        System.out.println(Arrays.toString(rsl));
     }
 }
