d2931f95dbfda126bcd9ae0aafc2f974edce799b
MaxChe94 <mchere94@gmail.com>
2019-11-14 00:24:49 +0500

    1.4. Вызов метод с возвращаемым типом.[#191961]

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,14 @@
 package ru.job4j.oop;
 
 public class Cat {
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
     public static void main (String[] args) {
-        Cat Peppy = new Cat();
-        Cat Sparky = new Cat();
+        Cat peppy = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
     }
 }
