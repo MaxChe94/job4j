ee740713bab17449ff6c1a22bf952e92a373d882
MaxChe94 <mchere94@gmail.com>
2019-11-13 22:38:57 +0500

    1.1. Создание объекта[#191956]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Cat {
+    public static void main (String[] args) {
+        Cat Peppy = new Cat();
+        Cat Sparky = new Cat();
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main (String[] args) {
+        Dog Polkan = new Dog();
+        Dog Sharik = new Dog();
+        Dog Zhychka = new Dog();
+    }
+}
