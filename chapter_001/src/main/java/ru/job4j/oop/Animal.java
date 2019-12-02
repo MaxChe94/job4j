d5a343eca59fe8c3683c0a73ce78af284e8d7119
MaxChe94 <mchere94@gmail.com>
2019-12-03 00:06:00 +0500

    1. Конструктор по умолчанию.[#191968]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Animal.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Animal {
+    public Animal(){
+        super();
+        System.out.println("This is class Animal");
+    }
+
+    public static void main (String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
