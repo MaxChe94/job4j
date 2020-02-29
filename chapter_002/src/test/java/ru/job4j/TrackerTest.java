912b158816638845c54602ad5b178f42cb7eaa68
MaxChe94 <mchere94@gmail.com>
2020-03-01 00:11:44 +0500

    1. Модульные тесты.[#191871]

--- chapter_002/src/test/java/ru/job4j/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/TrackerTest.java
@@ -3,37 +3,52 @@ package ru.job4j;
 import org.junit.Test;
 import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
+import org.junit.Assert;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
     @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
+    public void whenAddNewItemThenTrackerHasSameItem () {
         Tracker tracker = new Tracker();
         Item item = new Item("test1");
         tracker.add(item);
         Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
+        Assert.assertEquals(result.getName(), item.getName());
     }
 
     @Test
-    public void findByNameTest() {
+    public void findByNameTest () {
         Tracker tracker = new Tracker();
         String[] names = {"first", "second", "first", "third"};
-        String[] n1= {"first", "first"};
-        for (String i:
-             names) {
+        String[] n1 = {"first", "first"};
+        for (String i :
+                names) {
             tracker.add(new Item(i));
         }
         Item[] itms = tracker.findByName("first");
         String[] result = new String[itms.length];
-        for (int j = 0; j < itms.length; j++){
+        for (int j = 0; j < itms.length; j++) {
             if (itms[j].getName().equals("first")) {
                 result[j] = itms[j].getName();
             }
         }
-        assertEquals(result, n1);
+        Assert.assertEquals(result, n1);
     }
+
+    @Test
+    public void findAllTest () {
+        Tracker tracker = new Tracker();
+        String[] names = {"first", "second", "first", "third"};
+        for (String i :
+                names) {
+            tracker.add(new Item(i));
+        }
+        Item[] itms = tracker.findAll();
+        String[] result = new String[itms.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = itms[i].getName();
+        }
+        Assert.assertEquals(result, names);
+    }
+
 }
