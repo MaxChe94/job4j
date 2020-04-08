67a1e9aa413f1750fb8d11fa4090f81ce6e7b057
MaxChe94 <mchere94@gmail.com>
2020-04-08 23:24:45 +0500

    Правка 3. Интерфейс Input[#191933]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -1,11 +1,17 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
+
 public class StubInput implements Input {
-    public String askStr(String question) {
-        return null;
+    private Scanner scanner = new Scanner(System.in);
+
+    public String askStr (String question) {
+        System.out.println(question);
+        return scanner.nextLine();
     }
 
-    public int askInt(String question) {
-        return 0;
+    public int askInt (String question) {
+        System.out.println(question);
+        return Integer.valueOf(scanner.nextLine());
     }
 }
